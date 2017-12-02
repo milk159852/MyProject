a2931c6978ccf6cefa096112db1ca483ff89a2d5
milk159852 <34156965+milk159852@users.noreply.github.com>
2017-12-03 01:43:42 +0800

    AddProjectControllerAndDetailView

---
+++ NewProject/Controllers/ProductController.cs
@@ -0,0 +1,37 @@
+﻿using NewProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NewProject.Controllers
+{
+    public class ProductController : Controller
+    {
+        private Northwind _context;
+
+        public ProductController()
+        {
+            _context = new Northwind();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        // GET: Product
+        public ActionResult Index()
+        {
+            var result = _context.Products.ToList();
+            return View(result);
+        }
+
+        public ActionResult Detail(int id)
+        {
+            var result = _context.Products.SingleOrDefault(c => c.ProductID == id);
+            return View(result);
+        }
+    }
+}

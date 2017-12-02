8331ced2b396fab52681609ddaaeca8af4dd528f
milk159852 <34156965+milk159852@users.noreply.github.com>
2017-12-02 23:37:46 +0800

    AddDataBase

---
+++ NewProject/Models/Northwind.cs
@@ -0,0 +1,26 @@
+namespace NewProject.Models
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class Northwind : DbContext
+    {
+        public Northwind()
+            : base("name=Northwind")
+        {
+        }
+
+        public virtual DbSet<Categories> Categories { get; set; }
+        public virtual DbSet<Products> Products { get; set; }
+        public virtual DbSet<Suppliers> Suppliers { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Products>()
+                .Property(e => e.UnitPrice)
+                .HasPrecision(19, 4);
+        }
+    }
+}

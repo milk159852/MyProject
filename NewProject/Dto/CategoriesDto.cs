b6900d6f34aaef45fb2373627477221cd70b611e
milk159852 <34156965+milk159852@users.noreply.github.com>
2017-12-03 17:09:31 +0800

    AddDto

---
+++ NewProject/Dto/CategoriesDto.cs
@@ -0,0 +1,29 @@
+﻿using NewProject.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace NewProject.Dto
+{
+    public class CategoriesDto
+    {
+        [Key]
+        public int CategoryID { get; set; }
+
+        [Required]
+        [StringLength(15)]
+        public string CategoryName { get; set; }
+
+        [Column(TypeName = "ntext")]
+        public string Description { get; set; }
+
+        [Column(TypeName = "image")]
+        public byte[] Picture { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Products> Products { get; set; }
+    }
+}
---
+++ NewProject/Dto/ProductsDto.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace NewProject.Dto
+{
+    public class ProductsDto
+    {
+        [Key]
+        public int ProductID { get; set; }
+
+        [Required]
+        [StringLength(40)]
+        public string ProductName { get; set; }
+
+        public int? SupplierID { get; set; }
+
+        public int? CategoryID { get; set; }
+
+        [StringLength(20)]
+        public string QuantityPerUnit { get; set; }
+
+        [Column(TypeName = "money")]
+        public decimal? UnitPrice { get; set; }
+
+        public short? UnitsInStock { get; set; }
+
+        public short? UnitsOnOrder { get; set; }
+
+        public short? ReorderLevel { get; set; }
+
+        public bool Discontinued { get; set; }
+
+        public virtual CategoriesDto Categories { get; set; }
+
+        public virtual SuppliersDto Suppliers { get; set; }
+    }
+}
